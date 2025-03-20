e035755f79c386729b96f09f019d4b6ff161e7b9
ro'a <ruaa.shamali@gmail.com>
2025-03-20 14:40:49 +0200

    main

--- src/modules/auth/auth.router.js
+++ src/modules/auth/auth.router.js
@@ -2,8 +2,8 @@ import { Router } from "express";
 import { SignUp } from "./controller/auth.controller.js";
 const router = Router()
 router.post('/signup',SignUp);
-router.post('/user', (req, res)=> { 
-    res.json({massage:'Hello World'})
-  })
+// router.post('/user', (req, res)=> { 
+//     res.json({massage:'Hello World'})
+//   })
 
 export default router
--- src/servies/sendEmail.js
+++ src/servies/sendEmail.js
@@ -1,1 +1,20 @@
 // nodemailer 
+import nodemailer from "nodemailer";
+export  async function sendEmail(){
+    const transporter = nodemailer.createTransport({
+        service:"gmail",
+        auth: {
+          user: "roa@gmail,com",
+          pass: "jn7jnAPss4f63QBp6D",
+        },
+      });
+      const info = await transporter.sendMail({
+          from: '"Maddison Foo Koch 👻" <maddison53@ethereal.email>', // sender address
+          to: "rua", // list of receivers
+          subject: "Hello ✔", // Subject line
+          text: "Hello world?", // plain text body
+          html: "<h2> hi</h2>", // html body
+        });
+      
+}
+

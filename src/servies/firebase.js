e30e64a8e8a76a6db234c3302d5fb5406e0a81f8
ro'a <ruaa.shamali@gmail.com>
2025-06-12 23:50:49 +0300

    main

--- src/servies/firebase.js
+++ src/servies/firebase.js
@@ -1,6 +1,21 @@
 import admin from "firebase-admin";
 
-import  serviceAccount from "./accountKey.json" with {type:"json"};
+// import  serviceAccount from "./accountKey.json" with {type:"json"};
+import dotenv from 'dotenv'
+dotenv.config();
+const serviceAccount={
+  type: process.env.TYPE,
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_ID,
+  private_key: process.env.PRIVATE_KEY,
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URL,
+  token_uri: process.env.TOKEN_URL,
+  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER,
+  client_x509_cert_url: process.env.CLIENT_URL,
+  universe_domain: process.env.UNIVERSE_DOMAIN,
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
